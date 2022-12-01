gingersnap-project/db-syncer

RHAM-84 Implement search solution

src/main/java/io/gingersnapproject/cdc/search/opensearch/OpenSearchIndexBackend.java
@@ -1,0 +1,123 @@
+package io.gingersnapproject.cdc.search.opensearch;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.ResponseException;
+import org.elasticsearch.client.ResponseListener;
+import org.elasticsearch.client.RestClient;
+import org.infinispan.commons.dataconversion.internal.Json;
+import org.jboss.logging.Logger;
+
+import io.gingersnapproject.cdc.SearchBackend;
+import io.gingersnapproject.cdc.translation.ColumnStringTranslator;
+import io.gingersnapproject.cdc.translation.JsonTranslator;
+import io.gingersnapproject.cdc.translation.PrependStringTranslator;
+
+public class OpenSearchIndexBackend implements SearchBackend {
+
+   private static final Logger LOG = Logger.getLogger(OpenSearchIndexBackend.class);
+
+   final RestClient restClient;
+   final String indexName;
+   final JsonTranslator<StringBuilder> keyTranslator;
+   final JsonTranslator<?> valueTranslator;
+
+   public OpenSearchIndexBackend(RestClient restClient, String indexName, JsonTranslator<?> keyTranslator,
+                                 JsonTranslator<?> valueTranslator) {
+      this.indexName = indexName;
+      this.restClient = restClient;
+
+      // TODO Support json keys
+      if (keyTranslator instanceof PrependStringTranslator && keyTranslator instanceof ColumnStringTranslator) {
+         this.keyTranslator = (JsonTranslator<StringBuilder>) keyTranslator;
+      } else {
+         throw new UnsupportedOperationException("json keys are not supported at the moment");
+      }
+
+      this.valueTranslator = valueTranslator;
+
+      // perform the init mapping synchronously
+      mapping();
+   }
+
+   @Override
+   public CompletionStage<Void> remove(Json json) {
+      String key = keyTranslator.apply(json).toString();
+
+      Request request = new Request(
+            "DELETE",
+            "/" + indexName + "/_doc/" + key);
+
+      return submit(request);
+   }
+
+   @Override
+   public CompletionStage<Void> put(Json json) {
+      String key = keyTranslator.apply(json).toString();
+      String value = valueTranslator.apply(json).toString();
+
+      Request request = new Request(
+            "PUT",
+            "/" + indexName + "/_doc/" + key);
+      request.setJsonEntity(value);
+
+      return submit(request);
+   }
+
+   private void mapping() {
+      Request request = new Request("PUT", "/" + indexName);
+
+      Json mappings = Json.object("mappings")
+            .set("_source", Json.object()
+                  .set("enabled", false));
+
+      request.setJsonEntity(mappings.toString());
+
+      try {
+         Response response = restClient.performRequest(request);
+
+         LOG.info("Index created: " + EntityUtils.toString(response.getEntity()));
+      } catch (ResponseException ex) {
+         LOG.info("Index already present: " + ex.getMessage());
+      } catch (IOException ex) {
+         throw new RuntimeException(ex);
+      }
+   }
+
+   private final CompletionStage<Void> submit(Request request) {
+      OpenSearchResponseListener responseListener = new OpenSearchResponseListener();
+      restClient.performRequestAsync(request, responseListener);
+      return responseListener.completionStage();
+   }
+
+   private static class OpenSearchResponseListener implements ResponseListener {
+      CompletableFuture<Void> future = new CompletableFuture<>();
+
+      @Override
+      public void onSuccess(Response response) {
+         future.complete(null);
+      }
+
+      @Override
+      public void onFailure(Exception exception) {
+         if ( exception instanceof ResponseException ) {
+            /*
+             * The client tries to guess what's an error and what's not, but it's too naive.
+             * A 404 on DELETE is not always important to us, for instance.
+             * Thus we ignore the exception and do our own checks afterwards.
+             */
+            future.complete( null );
+         }
+         future.completeExceptionally(exception);
+      }
+
+      public CompletionStage<Void> completionStage() {
+         return future;
+      }
+   }
+}

src/main/java/io/gingersnapproject/cdc/search/opensearch/OpenSearchService.java
@@ -1,0 +1,42 @@
+package io.gingersnapproject.cdc.search.opensearch;
+
+import java.net.URI;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+
+import io.gingersnapproject.cdc.SearchBackend;
+import io.gingersnapproject.cdc.search.SearchService;
+import io.gingersnapproject.cdc.translation.JsonTranslator;
+
+public class OpenSearchService implements SearchService {
+
+   private static final String INDEX_NAME = "debezium-index";
+
+   @Override
+   public SearchBackend backendForURI(URI uri, JsonTranslator<?> keyTranslator, JsonTranslator<?> valueTranslator) {
+      RestClientBuilder builder = RestClient.builder(HttpHost.create(uri.toString()));
+      builder.setHttpClientConfigCallback(new BasicAuthClientConfig());
+      RestClient restClient = builder.build();
+
+      return new OpenSearchIndexBackend(restClient, INDEX_NAME, keyTranslator, valueTranslator);
+   }
+
+   public class BasicAuthClientConfig implements RestClientBuilder.HttpClientConfigCallback {
+
+      @Override
+      public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("admin", "admin"));
+
+         httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+         return httpClientBuilder;
+      }
+   }
+}

fax4ever/gingersnap-db-syncer

src/main/java/io/gingersnapproject/cdc/search/opensearch/OpenSearchIndexBackend.java
@@ -1,0 +1,123 @@
+package io.gingersnapproject.cdc.search.opensearch;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.ResponseException;
+import org.elasticsearch.client.ResponseListener;
+import org.elasticsearch.client.RestClient;
+import org.infinispan.commons.dataconversion.internal.Json;
+import org.jboss.logging.Logger;
+
+import io.gingersnapproject.cdc.IndexBackend;
+import io.gingersnapproject.cdc.translation.ColumnStringTranslator;
+import io.gingersnapproject.cdc.translation.JsonTranslator;
+import io.gingersnapproject.cdc.translation.PrependStringTranslator;
+
+public class OpenSearchIndexBackend implements IndexBackend {
+
+   private static final Logger LOG = Logger.getLogger(OpenSearchIndexBackend.class);
+
+   final RestClient restClient;
+   final String indexName;
+   final JsonTranslator<StringBuilder> keyTranslator;
+   final JsonTranslator<?> valueTranslator;
+
+   public OpenSearchIndexBackend(RestClient restClient, String indexName, JsonTranslator<?> keyTranslator,
+                                 JsonTranslator<?> valueTranslator) {
+      this.indexName = indexName;
+      this.restClient = restClient;
+
+      // TODO Support json keys
+      if (keyTranslator instanceof PrependStringTranslator && keyTranslator instanceof ColumnStringTranslator) {
+         this.keyTranslator = (JsonTranslator<StringBuilder>) keyTranslator;
+      } else {
+         throw new UnsupportedOperationException("json keys are not supported at the moment");
+      }
+
+      this.valueTranslator = valueTranslator;
+
+      // perform the init mapping synchronously
+      mapping();
+   }
+
+   @Override
+   public CompletionStage<Void> remove(Json json) {
+      String key = keyTranslator.apply(json).toString();
+
+      Request request = new Request(
+            "DELETE",
+            "/" + indexName + "/_doc/" + key);
+
+      return submit(request);
+   }
+
+   @Override
+   public CompletionStage<Void> put(Json json) {
+      String key = keyTranslator.apply(json).toString();
+      String value = valueTranslator.apply(json).toString();
+
+      Request request = new Request(
+            "PUT",
+            "/" + indexName + "/_doc/" + key);
+      request.setJsonEntity(value);
+
+      return submit(request);
+   }
+
+   private void mapping() {
+      Request request = new Request("PUT", "/" + indexName);
+
+      Json mappings = Json.object("mappings")
+            .set("_source", Json.object()
+                  .set("enabled", false));
+
+      request.setJsonEntity(mappings.toString());
+
+      try {
+         Response response = restClient.performRequest(request);
+
+         LOG.info("Index created: " + EntityUtils.toString(response.getEntity()));
+      } catch (ResponseException ex) {
+         LOG.info("Index already present: " + ex.getMessage());
+      } catch (IOException ex) {
+         throw new RuntimeException(ex);
+      }
+   }
+
+   private final CompletionStage<Void> submit(Request request) {
+      OpenSearchResponseListener responseListener = new OpenSearchResponseListener();
+      restClient.performRequestAsync(request, responseListener);
+      return responseListener.completionStage();
+   }
+
+   private static class OpenSearchResponseListener implements ResponseListener {
+      CompletableFuture<Void> future = new CompletableFuture<>();
+
+      @Override
+      public void onSuccess(Response response) {
+         future.complete(null);
+      }
+
+      @Override
+      public void onFailure(Exception exception) {
+         if ( exception instanceof ResponseException ) {
+            /*
+             * The client tries to guess what's an error and what's not, but it's too naive.
+             * A 404 on DELETE is not always important to us, for instance.
+             * Thus we ignore the exception and do our own checks afterwards.
+             */
+            future.complete( null );
+         }
+         future.completeExceptionally(exception);
+      }
+
+      public CompletionStage<Void> completionStage() {
+         return future;
+      }
+   }
+}

src/main/java/io/gingersnapproject/cdc/search/opensearch/OpenSearchService.java
@@ -1,0 +1,47 @@
+package io.gingersnapproject.cdc.search.opensearch;
+
+import java.net.URI;
+
+import javax.inject.Singleton;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+
+import io.gingersnapproject.cdc.IndexBackend;
+import io.gingersnapproject.cdc.search.SearchService;
+import io.gingersnapproject.cdc.translation.JsonTranslator;
+import io.quarkus.arc.lookup.LookupIfProperty;
+
+@LookupIfProperty(name = "service.opensearch.enabled", stringValue = "true", lookupIfMissing = true)
+@Singleton
+public class OpenSearchService implements SearchService {
+
+   private static final String INDEX_NAME = "debezium-index";
+
+   @Override
+   public IndexBackend backendForURI(URI uri, JsonTranslator<?> keyTranslator, JsonTranslator<?> valueTranslator) {
+      RestClientBuilder builder = RestClient.builder(HttpHost.create(uri.toString()));
+      builder.setHttpClientConfigCallback(new BasicAuthClientConfig());
+      RestClient restClient = builder.build();
+
+      return new OpenSearchIndexBackend(restClient, INDEX_NAME, keyTranslator, valueTranslator);
+   }
+
+   public class BasicAuthClientConfig implements RestClientBuilder.HttpClientConfigCallback {
+
+      @Override
+      public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("admin", "admin"));
+
+         httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+         return httpClientBuilder;
+      }
+   }
+}